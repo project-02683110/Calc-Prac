23f14e829e092c6d75708ee97708e45d6dd10a2e
E-Project <project2683110@gmail.com>
2023-08-11 01:25:47 +0900

    qadd

--- physics-elc.js
+++ physics-elc.js
@@ -1,20 +1,34 @@
+let QuestionsData = [
+    '面積<button class="tile">S</button>, 極板間隔<button class="tile">d</button>のコンデンサーCがある.<br>Cに電圧<span class="math">V</span>を加えて十分時間が経ったとき, Cには<button class="tile">Q</button>の電荷がたくわえられた.<br>このとき以下の式が成り立つ.<blockquote class="math">V=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac>, Q=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac>V</blockquote>ここで極板間に生じる電場<span class="math">E</span>は<span class="math">E=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac></span>となるので, その大きさは<span class="math">d</span>によらない.<br>またこのとき, 誘電比率<span class="math">ε</span>は<span class="math">ε=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac></span>で表される.<br>ただしクーロンの法則の比例定数を<button class="tile">k</button>, 円周率を<button class="tile">π</button>とする.<div id="secret-answer">0:Num:4,Q:1,d:1,k:1,π:1;1:Num:1,S:1;2:Num:1,S:1;3:Num:4,d:1,k:1,π:1;4:Num:4,Q:1,k:1,π:1;5:Num:1,S:1;6:Num:1;7:Num:4,k:1,π:1</div>',
+    '断面積<button class="tile">S</button>, 長さ<button class="tile">l</button>の抵抗に一定の電圧<button class="tile">V</button>をかける.<br>このとき抵抗内に存在する電気量<span class="math">－<button class="tile">e</button></span>の電子に電場から<span class="math">F<sub class="math">1</sub>=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac></span>の力を受ける.<br>また電子はこれとは逆の方向に, その速さ<span class="math">v</span>に比例した<span class="math">F<sub class="math">2</sub>=<button class="tile">k</button>v</span>の抵抗力を受ける.<br>この2力が釣り合うとき, 電子の速さは<span class="math">v=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac></span><br>よって抵抗内の電子密度を<button class="tile">n</button>とすると以下が成り立つ.<blockquote class="math">I=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac>V, V=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac>I</blockquote>またこのとき抵抗の抵抗率<span class="math">ρ</span>は<span class="math">ρ=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac></span><div id="secret-answer">0:Num:1,V:1,e:1;1:Num:1,l:1;2:Num:1,V:1,e:1;3:Num:1,k:1,l:1;4:Num:1,S:1,e:2,n:1;5:Num:1,k:1,l:1;6:Num:1,k:1,l:1;7:Num:1,S:1,e:2,n:1;8:Num:1,k:1;9:Num:1,e:2,n:1</div>',
+    '単位長あたりの抵抗が<button class="tile">r</button>で, 長さが<button class="tile">L</button>の一様な直線状の抵抗線ABがある.<br>両端ABに起電力<button class="tile">E</button>の直流電源の正極と負極をそれぞれつなぐいだとき, ABを流れる電流の大きさは<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac>である.<br>また抵抗線上にAT<span class="math">=</span><button class="tile">a</button>, BT<span class="math">=</span><button class="tile">b</button>となるよう接点Tをとる.<br>A, Tを起電力<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac>の直流電源の正極と負極につないだとき, 電源には電流が流れなかった.<div id="secret-answer">0:E:1,Num:1;1:L:1,Num:1,r:1;2:E:1,Num:1,a:1;3:L:1,Num:1</div>'
+]
+
 let global;
+let history = [...Array(QuestionsData.length)].map((_, i) => i.toString());
+let charge = false;
 let Completed = true;
-let A;
 
 window.onload = function() {
     reset();
+    document.addEventListener('keypress', function(e) {
+        console.log(history);
+        if (e.key == 's') reset();
+    });
 }
 
 function reset() {
     let Q = document.getElementById('Q');
     if (Completed) {
-        let a;
-        do {
-            a = Math.floor(Math.random() * QuestionsData.length);
-        } while (a == A)
-        A = a;
-        Q.innerHTML = QuestionsData[a];
+        if (history.length == 0) {
+            Q.innerHTML = '<h2>問題を一周しました</h2><br><button class="tile">R</button>をタップしてリスタート。<br><button class="input-box"></button><div id="secret-answer">0:Num:1,R:1</div>'
+            history = [...Array(3)].map((_, i) => i);
+            charge = true;
+        } else {
+            let n = Math.floor(Math.random() * history.length);
+            Q.innerHTML = QuestionsData[history[n]];
+            history.splice(n, 1);
+        }
     }
     let key = document.getElementsByClassName('tile');
     for (let i = 0; i < key.length; i++) {
@@ -50,16 +64,20 @@ function toutched(e) {
     console.log(ansData);
     var correct = document.getElementById('secret-answer');
     if (ansData == correct.textContent) {
-        var effect = document.getElementById('Effect');
-        var score = document.getElementById('score');
-        var S = Number(score.innerText);
-        S++;
-        if (S>999) S=999;
-        score.innerText = ( '000' + S ).slice( -3 );
-        if (effect.value == 'T') aPlay('T');
-        document.body.animate({
-            background: ["deepskyblue", "white"],
-        }, 500 );
+        if (charge) {
+            charge = false;
+        } else {
+            var effect = document.getElementById('Effect');
+            var score = document.getElementById('score');
+            var S = Number(score.innerText);
+            S++;
+            if (S>999) S=999;
+            score.innerText = ( '000' + S ).slice( -3 );
+            if (effect.value == 'T') aPlay('T');
+            document.body.animate({
+                background: ["deepskyblue", "white"],
+            }, 500 );
+        }
         reset();
     }
 }
@@ -121,9 +139,3 @@ function aPlay(file) {
     document.getElementById(file).currentTime = 0;
     document.getElementById(file).play();
 }
-
-
-let QuestionsData = [
-    '面積<button class="tile">S</button>, 極板間隔<button class="tile">d</button>のコンデンサーCがある.<br>Cに電圧<span class="math">V</span>を加えて十分時間が経ったとき, Cには<button class="tile">Q</button>の電荷がたくわえられた.<br>このとき以下の式が成り立つ.<blockquote class="math">V=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac>, Q=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac>V</blockquote>ここで極板間に生じる電場<span class="math">E</span>は<span class="math">E=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac></span>となるので, その大きさは<span class="math">d</span>によらない.<br>またこのとき, 誘電比率<span class="math">ε</span>は<span class="math">ε=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac></span>で表される.<br>ただしクーロンの法則の比例定数を<button class="tile">k</button>, 円周率を<button class="tile">π</button>とする.<div id="secret-answer">0:Num:4,Q:1,d:1,k:1,π:1;1:Num:1,S:1;2:Num:1,S:1;3:Num:4,d:1,k:1,π:1;4:Num:4,Q:1,k:1,π:1;5:Num:1,S:1;6:Num:1;7:Num:4,k:1,π:1</div>',
-    '断面積<button class="tile">S</button>, 長さ<button class="tile">l</button>の抵抗に一定の電圧<button class="tile">V</button>をかける.<br>このとき抵抗内に存在する電気量<span class="math">－<button class="tile">e</button></span>の電子に電場から<span class="math">F<sub class="math">1</sub>=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac></span>の力を受ける.<br>また電子はこれとは逆の方向に, その速さ<span class="math">v</span>に比例した<span class="math">F<sub class="math">2</sub>=<button class="tile">k</button>v</span>の抵抗力を受ける.<br>この2力が釣り合うとき, 電子の速さは<span class="math">v=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac></span><br>よって抵抗内の電子密度を<button class="tile">n</button>とすると以下が成り立つ.<blockquote class="math">I=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac>V, V=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac>I</blockquote>またこのとき抵抗の抵抗率<span class="math">ρ</span>は<span class="math">ρ=<frac><fn><button class="input-box"></button></fn><fd><button class="input-box"></button></fd></frac></span><div id="secret-answer">0:Num:1,V:1,e:1;1:Num:1,l:1;2:Num:1,V:1,e:1;3:Num:1,k:1,l:1;4:Num:1,S:1,e:2,n:1;5:Num:1,k:1,l:1;6:Num:1,k:1,l:1;7:Num:1,S:1,e:2,n:1;8:Num:1,k:1;9:Num:1,e:2,n:1</div>'
-]
